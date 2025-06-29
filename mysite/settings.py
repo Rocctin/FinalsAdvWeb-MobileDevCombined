eb5cf887e1279783ed81c377d577c899db2497ec
MarcusLoooo <ljhlwq.noble6@gmail.com>
2025-06-30 00:55:14 +0800

    Fixed Herokuy Deployment Configuration. Should have resolved error code = H14

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y25=_4-top0z1ngvscf2-q@i))_5mq&s=r96-1-7yed#ccehv@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://drfcm3035marcusapi-01c8a6c03e5d.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['drfcm3035marcusapi-01c8a6c03e5d.herokuapp.com', 'markylooooo.rocks', 'www.markylooooo.rocks', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
